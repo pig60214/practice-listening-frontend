8b5016d271d4e02b0d00a099e2ffcc954858ddea
YX <pig60214@gmail.com>
2023-12-25 22:23:01 -0500

    Adjust Update Page

--- src/views/updateTranscription.tsx
+++ src/views/updateTranscription.tsx
@@ -43,12 +43,14 @@ export default function UpdateTranscription() {
   }
   return (
     <>
-    <div className="flex flex-col">
-      <input value={transcription.title} onChange={(e) => setTranscription({...transcription, title: e.target.value})} placeholder="Title" />
-      <input value={transcription.youtubeUrl} onChange={(e) => setTranscription({...transcription, youtubeUrl: e.target.value})} placeholder="Youtube Url" />
-      <button onClick={fetchYoutubeTranscription} disabled={retreving}>Retrieve Title & Transcript From Youtube</button>
-      <textarea value={transcription.content} onChange={(e) => setTranscription({...transcription, content: e.target.value})} placeholder="Transcript"></textarea>
-      <button onClick={saveTranscription} disabled={saving}>save</button>
+    <div className="grid gap-2 justify-items-stretch max-w-3xl mx-4 md:mx-auto">
+      <div className="flex gap-2">
+      <input className="grow textbox" value={transcription.youtubeUrl} onChange={(e) => setTranscription({...transcription, youtubeUrl: e.target.value})} placeholder="Youtube Url" />
+      <button className="button secondary md:w-fit justify-self-center" onClick={fetchYoutubeTranscription} disabled={retreving}>Retrieve</button>
+      </div>
+      <input className="textbox" value={transcription.title} onChange={(e) => setTranscription({...transcription, title: e.target.value})} placeholder="Title" />
+      <textarea className="textbox" value={transcription.content} onChange={(e) => setTranscription({...transcription, content: e.target.value})} placeholder="Transcript"></textarea>
+      <button className="button md:w-fit justify-self-center mt-3" onClick={saveTranscription} disabled={saving}>save</button>
     </div>
     </>
   );
