d957799c122fc6801696695fb50f42cce952b024
YX <pig60214@gmail.com>
2023-12-26 22:14:04 -0500

    Add Loading Mask

--- src/views/updateTranscription.tsx
+++ src/views/updateTranscription.tsx
@@ -7,49 +7,59 @@ import { useParams } from "react-router-dom";
 export default function UpdateTranscription() {
   const { transcriptionId } = useParams();
   const [transcription, setTranscription] = useState<IUpdateTranscription>({title: '', youtubeUrl: '', content: ''} as IUpdateTranscription);
-  const [saving, setSaving] = useState<boolean>(false);
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const getTranscription = async () => {
       if (transcriptionId) {
+        setIsLoading(true);
         const t = (await apis.getTranscription(Number(transcriptionId))).data;
-        if (!t) {
-          return;
+        if (t) {
+          setTranscription(t);
         }
-        setTranscription(t);
+        setIsLoading(false);
       }
     }
     getTranscription();
   }, [transcriptionId])
 
   const saveTranscription = async () => {
-    setSaving(true);
+    setIsSaving(true);
     if(transcriptionId) {
       await apis.updateTranscription(transcription)
     } else {
       await apis.addTranscription(transcription)
     }
-    setSaving(false);
+    setIsSaving(false);
   }
-  const [retreving, setRetreving] = useState(false);
+  const [isRetreving, setisRetreving] = useState(false);
   const fetchYoutubeTranscription =async () => {
-    setRetreving(true);
+    setisRetreving(true);
     const request: FetchYoutubeTranscriptionRequest = {
       youtubeUrl: transcription.youtubeUrl,
     }
     const result = await apis.fetchYoutubeTranscription(request);
     setTranscription({...transcription, title: result.data.title, content: JSON.stringify(result.data.transcript)});
-    setRetreving(false);
+    setisRetreving(false);
   }
   return (
+    <>
     <div className="grid gap-2 justify-items-stretch max-w-3xl mt-4 md:mt-0 mx-4 md:mx-auto">
       <div className="flex gap-2">
       <input className="grow textbox" value={transcription.youtubeUrl} onChange={(e) => setTranscription({...transcription, youtubeUrl: e.target.value})} placeholder="Youtube Url" />
-      <button className="button secondary md:w-fit justify-self-center" onClick={fetchYoutubeTranscription} disabled={retreving}>Retrieve</button>
+      <button className="button secondary md:w-fit justify-self-center" onClick={fetchYoutubeTranscription} disabled={isRetreving}>Retrieve</button>
       </div>
       <input className="textbox" value={transcription.title} onChange={(e) => setTranscription({...transcription, title: e.target.value})} placeholder="Title" />
       <textarea className="textbox" value={transcription.content} onChange={(e) => setTranscription({...transcription, content: e.target.value})} placeholder="Transcript"></textarea>
-      <button className="button md:w-fit justify-self-center mt-3" onClick={saveTranscription} disabled={saving}>save</button>
+      <button className="button md:w-fit justify-self-center mt-3" onClick={saveTranscription} disabled={isSaving}>save</button>
     </div>
+    {
+      (isLoading || isRetreving || isSaving) &&
+      <div className="w-full h-full z-10 absolute top-0 bg-stone-100 opacity-70 grid place-content-center">
+        <div className="animate-pulse">● ● ●</div>
+      </div>
+    }
+    </>
   );
 }
